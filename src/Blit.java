816c1c1789da6ca23b2462e44978c1db6454ba8a
sl4cks <10481627+sl4cks@users.noreply.github.com>
2019-03-20 10:52:06 -0400

    Fixed brackets

--- src/Blit.java
+++ src/Blit.java
@@ -1,25 +1,22 @@
-public class Blit extends Osc
-	{
-	public double sincm(double x, double m)
-		{
+public class Blit extends Osc {
+
+	public double sincm(double x, double m) {
 		double v = m * Math.sin(Math.PI * x / m);
 		if (v == 0) return 1;
 		return Math.sin(Math.PI * x) / v;
-		}
+	}
 	
 	double phase = 0;
 	
-	public double tick(long tickCount)
-		{
+	public double tick(long tickCount) {
 		return tick(tickCount, 0);
-		}
+	}
 		
 		
 	// Note that BLIT will generate values below 0.  That's okay I guess, we're not using it
 	// as a serious audio output source, just scaled derivatives of it like saw.
 
-    protected double tick(long tickCount, double d)
-    	{
+    protected double tick(long tickCount, double d) {
     	double freq = Utils.valueToHz(getFrequencyMod().getValue());
     	double p = Config.SAMPLING_RATE / freq;
     	double m = Math.floor(p / 2.0) * 2.0 + 1.0;
