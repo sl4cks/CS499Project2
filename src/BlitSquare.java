35adde88e9ceada0ee2f6c54924f5d39836f2b70
sl4cks <10481627+sl4cks@users.noreply.github.com>
2019-03-19 21:47:24 -0400

    Rough implementation of blitsquare

--- src/BlitSquare.java
+++ src/BlitSquare.java
@@ -1,2 +1,18 @@
-public class BlitSquare {
+public class BlitSquare extends BPBlit {
+
+    private double prev = 0;
+
+    public double tick(long tickCount) {
+        double val;
+        if(tickCount <= 0)
+            val = 0;
+        else {
+            double alpha = 0.999;
+            double freq = Utils.valueToHz(getFrequencyMod().getValue());
+            double p = Config.SAMPLING_RATE / freq;
+            val = alpha * prev + super.tick(tickCount);
+        }
+        prev = val;
+        return val;
+    }
 }
