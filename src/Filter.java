153f74da32cbb1b8a29bd3f152c7c8e0c30cd56a
sl4cks <10481627+sl4cks@users.noreply.github.com>
2019-03-19 21:59:13 -0400

    Added Filter constructor

--- src/Filter.java
+++ src/Filter.java
@@ -8,6 +8,13 @@ public class Filter extends Module {
     private double[] y;
     private double x0;
 
+    public Filter(Module input, double[] a, double[] b, double b0) {
+        this.input = input;
+        this.a = a;
+        this.b = b;
+        this.b0 = b0;
+    }
+
     public double tick(long tickCount) {
         return 0.0;
     }
