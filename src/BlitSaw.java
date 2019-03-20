5d246f8c53c9e9059bd94be3472c2ab686c2d11b
sl4cks <10481627+sl4cks@users.noreply.github.com>
2019-03-19 21:44:13 -0400

    Added piecewise 0

--- src/BlitSaw.java
+++ src/BlitSaw.java
@@ -3,10 +3,15 @@ public class BlitSaw extends Blit {
     private double prev = 0;
 
     public double tick(long tickCount) {
-        double freq = Utils.valueToHz(getFrequencyMod().getValue());
-        double p = Config.SAMPLING_RATE / freq;
-        double alpha = 1 - (1/p);
-        double val = alpha * prev + super.tick(tickCount) - (1/p);
+        double val;
+        if(tickCount <= 0)
+            val = 0;
+        else {
+            double freq = Utils.valueToHz(getFrequencyMod().getValue());
+            double p = Config.SAMPLING_RATE / freq;
+            double alpha = 1 - (1 / p);
+            val = alpha * prev + super.tick(tickCount) - (1 / p);
+        }
         //change previous value to the new value
         prev = val;
         return val;
