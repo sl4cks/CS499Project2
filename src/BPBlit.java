c694dff5f6544d6fc2d4ba10eb2bc12b6eb3bb56
sl4cks <10481627+sl4cks@users.noreply.github.com>
2019-03-19 21:45:44 -0400

    Corrected BPBlit

--- src/BPBlit.java
+++ src/BPBlit.java
@@ -1,20 +1,17 @@
 public class BPBlit extends Blit {
 
     private double duty = .5;
-    private double prev = 0;
-    final private double alpha = 0.999;
 
     public double tick(long tickCount) {
         double val;
-        if(tickCount == 0)
+        if(tickCount <= 0)
             val = 0;
         else {
             double freq = Utils.valueToHz(getFrequencyMod().getValue());
             double p = Config.SAMPLING_RATE / freq;
-            val = alpha * prev + super.tick(tickCount) - super.tick((long) (tickCount - p * duty)); //may call blit with negative
+            val = super.tick(tickCount) - super.tick((long)(tickCount-p*duty));
         }
         //update previous, return new value
-        prev = val;
         return val;
     }
 }
