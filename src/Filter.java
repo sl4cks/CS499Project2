b17a8841ade0526c2d6b862bf9e209271e9e9371
sl4cks <10481627+sl4cks@users.noreply.github.com>
2019-03-19 21:55:49 -0400

    Added filter field variables

--- src/Filter.java
+++ src/Filter.java
@@ -1,5 +1,13 @@
 public class Filter extends Module {
 
+    private Module input;
+    private double b0;
+    private double[] b;
+    private double[] a;
+    private double[] x;
+    private double[] y;
+    private double x0;
+
     public double tick(long tickCount) {
         return 0.0;
     }
