58c1852c8f9910ad0e3d6c8563cbc95fe9f410ba
sl4cks <10481627+sl4cks@users.noreply.github.com>
2019-03-19 21:20:02 -0400

    Rough implementation of BPBlit

--- src/BPBlit.java
+++ src/BPBlit.java
@@ -1,2 +1,20 @@
 public class BPBlit extends Blit {
+
+    private double duty = .5;
+    private double prev = 0;
+    final private double alpha = 0.999;
+
+    public double tick(long tickCount) {
+        double val;
+        if(tickCount == 0)
+            val = 0;
+        else {
+            double freq = Utils.valueToHz(getFrequencyMod().getValue());
+            double p = Config.SAMPLING_RATE / freq;
+            val = alpha * prev + super.tick(tickCount) - super.tick((long) (tickCount - p * duty)); //may call blit with negative
+        }
+        //update previous, return new value
+        prev = val;
+        return val;
+    }
 }
