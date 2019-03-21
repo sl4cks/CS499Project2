cdb616acc392d446d6d019fa2dd8204b6dd0e486
Alison Howland <alisonhowland@gmail.com>
2019-03-21 10:46:45 -0400

    Removed ResononceMod class, added LPF methods

--- src/LPF.java
+++ src/LPF.java
@@ -1,8 +1,24 @@
 public class LPF extends Filter {
 
-    private Module cutoff = new ConstantValue(0.0); // default
+    private Module cutoff =  new ConstantValue(Utils.hzToValue(0));  // default no cutoff
+    private Module resonanceMod = new ConstantValue(0.0);
 
     public LPF(Module input, double[] a, double[] b, double b0) {
         super(input, a, b, b0);
     }
+
+    public void setCutoff(Module frequency) {
+        this.cutoff = frequency;
+    }
+
+    public void setResonance (Module resonance) {
+        this.resonanceMod = resonance;
+    }
+
+    // Returns the resonance (Q) as a value from 1/sqrt(2) to ~10.0
+    public double getQ() {
+        return resonanceMod.getValue() * 10 + 1 / Math.sqrt(2);
+    }
+   // Algorithm 14
+
 }
--- src/ResonanceMod.java
+++
@@ -1,7 +0,0 @@
-public class ResonanceMod extends Module {
-
-    @Override
-    public double tick(long tickCount) {
-        return 0;
-    }
-}
