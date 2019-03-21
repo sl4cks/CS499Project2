9895d67f853d676020f058bdf0ffb646f8e968a7
Alison Howland <alisonhowland@gmail.com>
2019-03-20 22:11:20 -0400

    Update Filter constructor, comments for clarity

--- src/Filter.java
+++ src/Filter.java
@@ -1,14 +1,19 @@
 public class Filter extends Module {
 
+    // Note: Referencing Algorithm 13/14, Filters notes
     private Module input;
     private double b0;
     private double[] b;
     private double[] a;
-    private double[] x;
-    private double[] y;
-    private double x0;
+    private double[] x;  // for holding input at time i
+    private double[] y;  // for holding output at time i
+    private double x0;   // current input
 
     public Filter(Module input, double[] a, double[] b, double b0) {
+        // Initialize x and y array elements all to zero
+        x = new double[a.length];
+        y = new double[b.length];
+
         this.input = input;
         this.a = a;
         this.b = b;
