d75ca4b2b6fa9062e43a43032da85d676c047454
sl4cks <10481627+sl4cks@users.noreply.github.com>
2019-03-22 13:47:48 -0400

    Change square

--- src/BlitSquare.java
+++ src/BlitSquare.java
@@ -2,17 +2,14 @@ public class BlitSquare extends BPBlit {
 
     private double prev = 0;
 
-    public double tick(long tickCount) {
-        double val;
-        if(tickCount <= 0)
-            val = 0;
-        else {
-            double alpha = 0.999;
-            double freq = Utils.valueToHz(getFrequencyMod().getValue());
-            double p = Config.SAMPLING_RATE / freq;
-            val = alpha * prev + super.tick(tickCount);
-        }
+    public double square(long tickCount) {
+        double alpha = 0.999;
+        double val = alpha * prev + super.bpblit(tickCount);
         prev = val;
         return val;
+    }
+
+    public double tick(long tickCount) {
+        return square(tickCount) * .75 + phase;
     }
 }
