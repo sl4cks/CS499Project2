94a00eb3ef50387e2768793d3d04e2d443ccb2c6
sl4cks <10481627+sl4cks@users.noreply.github.com>
2019-03-22 10:31:19 -0400

    Fixed BPBlit

--- src/BPBlit.java
+++ src/BPBlit.java
@@ -3,27 +3,24 @@ public class BPBlit extends Blit {
     private double duty = .5;
     private Blit offPhaseBlit = new Blit();
 
-    public Module getPhaseMod() {
-        return null;
+    public double getPhaseMod() {
+        return this.duty;
     }
 
-    public void setPhaseMod(Module mod) {
+    public void setPhaseMod(double phaseMod) {
+        this.duty = phaseMod;
+    }
+
+    public void setFrequencyMod(Module module) {
+        super.setFrequencyMod(module);
+        offPhaseBlit.setFrequencyMod(module);
+    }
+
+    public double bpblit(long tickCount) {
+        return super.tick(tickCount) - offPhaseBlit.tick(tickCount, getPhaseMod());
     }
 
     public double tick(long tickCount) {
-        double val;
-        if(tickCount <= 0)
-            val = 0;
-        else {
-            double freq = Utils.valueToHz(getFrequencyMod().getValue());
-            double p = Config.SAMPLING_RATE / freq;
-
-            if(tickCount - p*duty < 0)
-                val = super.tick((long)(tickCount+p*(1-duty)));
-            else
-                val = super.tick(tickCount) - offPhaseBlit.tick((long)(tickCount-p*duty));
-        }
-        //update previous, return new value
-        return val;
+        return bpblit(tickCount)*0.5+0.5;
     }
 }
