3f1fd54d3fa358ec840cc9c417baa9b0b5c3e491
sl4cks <10481627+sl4cks@users.noreply.github.com>
2019-03-22 21:30:05 -0400

    Added singlular setInput and setAmplitude

--- src/MixerModule.java
+++ src/MixerModule.java
@@ -2,9 +2,6 @@
  * Implement a mixer which takes three inputs I1, I2, I3, and three
  * amplitudes A1, A2, A3, and performs I1 * A1 + I2 * A2 + I3 * A3.
  */
-
-
-
 public class MixerModule extends Module {
 
     private int size = 3;
@@ -23,11 +20,21 @@ public class MixerModule extends Module {
         }
    }
 
-   public void setAmplitudes(Module[] amplitudes) {
+   public void setInput(Module input, int i) {
+        if(i >= size)
+            throw new IllegalArgumentException("Input number exceeds size of mixer.");
+        this.input[i] = input;
+   }
+
+   public void setAmplitude(Module[] amplitudes) {
         this.amplitudes = new Module[amplitudes.length];
         for (int i = 0; i < amplitudes.length; i++) {
             this.amplitudes[i] = amplitudes[i];
         }
+   }
+
+   public void setAmplitude(Module amp, int i) {
+        this.amplitudes[i] = amp;
    }
 
     @Override
@@ -36,7 +43,6 @@ public class MixerModule extends Module {
         for (int i = 0; i < size; i ++) {
             output += input[i].getValue() * amplitudes[i].getValue();
         }
-        setValue(output);
         return output;
     }
 }
