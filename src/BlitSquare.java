ebbf7a38971ad3c53a6a92e76d638f58616b3543
sl4cks <10481627+sl4cks@users.noreply.github.com>
2019-03-24 16:13:12 -0400

    Removed square scaling

--- src/BlitSquare.java
+++ src/BlitSquare.java
@@ -1,16 +1,15 @@
 public class BlitSquare extends BPBlit {
 
     private double prev = 0;
+    private double alpha = 0.999;
 
     public double square(long tickCount) {
-        double alpha = 0.999;
-        //super updates prev and gives bpblit value
-        return alpha * prev + super.bpblit(tickCount);
+        return alpha * prev + bpblit(tickCount);
     }
 
     public double tick(long tickCount) {
         //update the previous value
         prev = square(tickCount);
-        return  prev * .75 + super.getPhaseMod();
+        return prev;
     }
 }
