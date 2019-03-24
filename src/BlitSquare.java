6731402d232f0aa35e855233bed1c0125cdc43bd
sl4cks <10481627+sl4cks@users.noreply.github.com>
2019-03-24 15:41:20 -0400

    Fixed 0 freq behavior on blitsquare

--- src/BlitSquare.java
+++ src/BlitSquare.java
@@ -1,16 +1,17 @@
 public class BlitSquare extends BPBlit {
 
     private double prev = 0;
+    //maintain separate previous value for the square
 
     public double square(long tickCount) {
         double alpha = 0.999;
-        double val = alpha * prev + super.bpblit(tickCount);
-        return val;
+        //super updates prev and gives bpblit value
+        return alpha * prev + super.bpblit(tickCount);
     }
 
     public double tick(long tickCount) {
         //update the previous value
         prev = square(tickCount);
-        return prev * .75 + super.getPhaseMod();
+        return  prev * .75 + super.getPhaseMod();
     }
 }
