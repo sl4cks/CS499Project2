c6f31a9e8346cecc4e052037569fe6115677508e
Alison Howland <alisonhowland@gmail.com>
2019-03-24 16:49:06 -0400

    Add detune function to Osc

--- src/Osc.java
+++ src/Osc.java
@@ -4,6 +4,9 @@
 public class Osc extends Module {
   public double state = 0;
 
+  // Module that provides the amount by which to detune the Osc's frequency
+  private Module detuneMod = new ConstantValue(0.0);
+
   private Module frequencyMod = new ConstantValue(Utils.hzToValue(220));
 
   public void setFrequencyMod(Module frequencyMod) {
@@ -12,6 +15,11 @@ public class Osc extends Module {
 
   public Module getFrequencyMod() {
     return this.frequencyMod;
+  }
+
+  public void setDetuneMod(Module detuneMod) {
+    this.detuneMod = detuneMod;
+//    double scale = (detuneMod.getValue() -0.5) * 2
   }
 
   public double tick(long tickCount) {
